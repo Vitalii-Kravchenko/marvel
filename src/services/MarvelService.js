33a6360acd724315295f83655fd060fa729b7bc6
Vitalii Kravchenko <vitalii.kravchenko.work@gmail.com>
2024-05-12 23:47:27 +0200

    Added: ability to get a random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,24 +1,38 @@
 class MarvelService {
-   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-   _apiKey = 'b6a19c75a2b88853dd1b6655904ec0b5';
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'b6a19c75a2b88853dd1b6655904ec0b5';
 
-   getResource = async (url) => {
-      let res = await fetch(url);
+    getResource = async (url) => {
+        let res = await fetch(url);
 
-      if (!res.ok) {
-         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-      }
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-      return await res.json();
-   }
+        return await res.json();
+    }
 
-   getAllCharacters = () => {
-      return this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
-   }
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
 
-   getCharacters = (id) => {
-      return this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
-   }
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (character) => {
+        return {
+            name: character.name,
+            description: character.description,
+            thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
+            homepage: character.urls[0].url,
+            wiki: character.urls[1].url,
+        }
+    }
 }
 
 export default MarvelService;
