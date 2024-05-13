0a547a6b97425c4d1d21a242caee10b08fb197d0
Vitalii Kravchenko <vitalii.kravchenko.work@gmail.com>
2024-05-13 02:18:19 +0200

    Fixed: bug of second request and memory leak

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,7 +9,6 @@ import mjolnir from '../../resources/img/mjolnir.png';
 class RandomChar extends Component {
     constructor(props) {
         super(props);
-        this.updateChar();
     }
 
     state = {
@@ -19,6 +18,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
 
     onCharLoaded = (char) => {
         this.setState({
