3d3c71361009086cc7c5d28359884292331f2065
Vitalii Kravchenko <vitalii.kravchenko.work@gmail.com>
2024-06-03 11:32:30 +0200

    Added: package react-helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,16 @@
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
+import {Helmet} from "react-helmet";
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="Page with list od our comics"
+                />
+                <title>Comics page</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
